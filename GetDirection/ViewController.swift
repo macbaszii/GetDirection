1ac16898a81a0cff389da9fde23915bf48c2715f
Bas <bas@oozou.com>
2015-03-12 16:39:01 +0700

    use core location to determine user current location

--- GetDirection/ViewController.swift
+++ GetDirection/ViewController.swift
@@ -14,13 +14,26 @@ class ViewController: UIViewController {
 
     @IBOutlet var latitudeField: UITextField!
     @IBOutlet var longitudeField: UITextField!
+    @IBOutlet var getDirectionButton: UIButton!
+    
+    var locationManager = CLLocationManager()
+    var currentCoordinate: CLLocationCoordinate2D?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getDirectionButton.enabled = false
+        setupCoreLocation()
     }
 }
 
 extension ViewController {
+    private func setupCoreLocation() {
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
     @IBAction func getDirection() {
         var chooseLocation = CLLocationCoordinate2DMake(latitudeField.text.toDouble() ?? 0.0, longitudeField.text.toDouble() ?? 0.0)
         
@@ -44,6 +57,22 @@ extension ViewController {
     
     private func getDirectionByGoogleMap(coodinate: CLLocationCoordinate2D) {
         
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == .Authorized || status == .AuthorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        manager.stopUpdatingLocation()
+        getDirectionButton.enabled = true
+        var currentLocation = locations.last as? CLLocation
+        currentCoordinate = currentLocation?.coordinate
     }
 }
 
